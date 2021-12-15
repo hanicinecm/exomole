fed97b9b7f568e49a2c1a012867c2714ff955f8e
Martin Hanicinec <hanicinecm@gmail.com>
2021-12-15 14:27:29 +0100

    implement read_trans

--- src/exomole/read_states.py
+++ src/exomole/read_states.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from .exceptions import StatesParseError
-from .utils import load_dataframe_chunks
+from .utils import load_dataframe_chunks, get_num_columns
 
 
 def states_chunks(states_path, chunk_size, columns):
@@ -9,10 +9,14 @@ def states_chunks(states_path, chunk_size, columns):
     Get a generator of chunks of the dataset .states file.
 
     Generator of pd.DataFrame chunks of the .states file, with
-    (hopefully correctly) assigned columns, and indexed by states indices
-    (the states indices are NOT present as a column, but as the dataframe index).
-    The columns of each chunk are as follows:
-    'E', 'g_tot', 'J' [, 'tau', 'g_J'], '<q_1>' [, '<q_2>', ..., '<q_N>']
+    rows indexed by the values of the first column in the .states file.
+    The columns argument passed needs to contain names for *all* the columns
+    *except the first* .states column, which is assumed to be the states index.
+    The generated DataFrames are cast explicitly to dtype=str, to avoid possible
+    nasty surprises caused by pandas guessing the types itself.
+    The columns can be re-casted downstream to the more appropriate data types
+    for faster processing. An example for the energy column is as follows:
+    state_chunk['E'] = state_chunk['col'].astype('float64')
 
     Parameters
     ----------
@@ -20,18 +24,34 @@ def states_chunks(states_path, chunk_size, columns):
         Path to the .states file on the local file system.
     chunk_size : int
         Chunk size, should be chosen appropriately with regards to RAM size.
+        Roughly 1_000_000 per 1GB consumed
     columns : list of str
-        Column names for all the columns in the states file. The first columns will
-        be the DataFrame index and must not be passed. Therefore, len(columns) must
-        be one less than the number of actual columns in the .states file.
+        Column names for all the columns in the states file except the first one,
+        which is assumed to be the states index.
+        Therefore, len(columns) must be one less than the number of actual columns
+        in the .states file.
 
     Yields
-    -------
+    ------
     states_chunk : pd.DataFrame
         Generated chunks of the states file, each is a pd.DataFrame with columns
         according to the argument passed, and indexed by the values in the first
         column in .states file.
+        The whole dataframe is of string type.
+
+    Raises
+    ------
+    StatesParseError
+        If len(columns) inconsistent with the number of columns in the .states file.
     """
+    states_path = Path(states_path)
+    num_cols = get_num_columns(states_path)
+    if num_cols != len(columns) + 1:
+        raise StatesParseError(
+            f"{len(columns)} column names passed, while "
+            f"{states_path.name} only {num_cols - 1} columns "
+            f"(excluding the index column)."
+        )
     chunks = load_dataframe_chunks(
         file_path=states_path,
         chunk_size=chunk_size,
@@ -42,7 +62,4 @@ def states_chunks(states_path, chunk_size, columns):
     for chunk in chunks:
         if list(chunk.columns) != columns[1:]:
             raise StatesParseError(f"Defense: {states_path}")
-        chunk.loc[:, "J"] = chunk.loc[:, "J"].astype("float64")
-        chunk.loc[:, "E"] = chunk.loc[:, "E"].astype("float64")
-        chunk.loc[:, "g_tot"] = chunk.loc[:, "g_tot"].astype("float64")
         yield chunk
