555e2f7e4e0d64c199c05ad3932e4644f66c8349
Martin Hanicinec <hanicinecm@gmail.com>
2022-02-10 02:52:18 +0100

    add the correct documentation url to setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
     },
     project_urls={
         "Bug Reports": "https://github.com/hanicinecm/exomole/issues",
-        "Documentation": "https://github.com/hanicinecm/exomole/tree/master/docs",
+        "Documentation": (
+            "https://github.com/hanicinecm/exomole/tree/master/docs/index.rst"
+        ),
     },
 )
